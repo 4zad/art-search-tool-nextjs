7bb2baef7426907735a0816c562bf206bf54648d
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-10-29 02:19:54 -0400

    fix: fixed errors in navbar component
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,48 +1,50 @@
-import { useState, useRouter } from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 import Link from 'next/link';
 import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
 
 export default function MainNav(props) {
   const router = useRouter();
-  const [searchField, setSearchField] = useState('Homer Simpson');
+  const [searchField, setSearchField] = useState('');
 
-  function submitSearchField() {
+  function submitSearchField(e) {
     e.preventDefault(); // prevent the browser from automatically submitting the form
     router.push(`/artwork?title=true&q=${searchField}`);
   }
 
   return (
     <>
-      {/* <Navbar className='fixed-top navbar-dark bg-dark'></Navbar> {/* same as "NavBar" component below *\/} */}
-      <Navbar className='fixed-top' bg='dark' variant='dark'>
+      {/* <Navbar className='fixed-top navbar-light bg-light'></Navbar> {/* same as "NavBar" component below *\/} */}
+      <Navbar className='fixed-top' bg='light' variant='light'>
         <Container fluid>
           <Navbar.Brand href='/'>Muhammad Ahmed</Navbar.Brand>
           <Navbar.Toggle aria-controls='navbarScroll' />
           <Navbar.Collapse id='navbarScroll'>
             <Nav className='me-auto'>
-              <Link href='/' passHref>
-                <Nav.Link>Home</Nav.Link>
-              </Link>
-              <Link href='/search' passHref>
-                <Nav.Link>Advanced Search</Nav.Link>
-              </Link>
+              <Nav.Link href='/'>Home</Nav.Link>
+              <Nav.Link href='/search'>Advanced Search</Nav.Link>
             </Nav>
-            <Form className='d-flex' onChange={(e) => setSearchField(e.target.value)} onSubmit={submitSearchField()}>
+            <Form
+              className='d-flex'
+              onChange={(e) => setSearchField(e.target.value)}
+              onSubmit={(e) => submitSearchField(e)}
+            >
               <Form.Control
                 className='me-2'
                 type='search'
                 placeholder='Search'
                 aria-label='Search field'
-                value={searchField}
+                defaultValue={searchField}
               />
-              <Button variant='outline-success' type='submit'>
+              <Button variant='outline-info' type='submit'>
                 Search
               </Button>
             </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <br />
       <br />
       <br />
     </>
