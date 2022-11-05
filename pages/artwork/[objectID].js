0d0b476b04a4c0f236ba67e155b5cd835ede62e5
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-11-04 22:37:13 -0400

    fix: 'router.query' returning null bug fixed
    - the name of the variable being assigned the value of 'router.query' must be identical to the string inside the square parenthesis in the file name (e.g. const { objectID } = router.query)
    
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- pages/artwork/[objectID].js
+++ pages/artwork/[objectID].js
@@ -6,21 +6,13 @@ import ArtworkCardDetail from '../../components/ArtworkCardDetail';
 
 export default function ObjectID() {
   let router = useRouter();
-  let objID;
-
-  useEffect(() => {
-    if (!router.isReady) return;
-
-    // codes using router.query
-    let { id } = router.query;
-    objID = id;
-  }, [router.isReady]);
+  let { objectID } = router.query;
 
   return (
     <>
       <Row>
         <Col>
-          <ArtworkCardDetail objectID={objID} />
+          <ArtworkCardDetail objectID={objectID} />
         </Col>
       </Row>
     </>
