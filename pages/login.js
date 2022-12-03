576a418a5403369466c2b95251dd1251bc528662
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-12-02 19:43:31 -0500

    feature: login page initialized
    - '/login' route and respective page are still untested
    
    - minor bug fix in 'userData.js' library
    
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

---
+++ pages/login.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { authenticateUser } from '../lib/authenticate';
+
+import { Card, Form, Button, Alert } from 'react-bootstrap';
+
+export default function Login(props) {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [warning, setWarning] = useState('');
+
+  const router = useRouter();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log('Form Submitted');
+
+    try {
+      await authenticateUser(userName, password);
+      router.push('/favorites');
+    } catch (err) {
+      //console.log(err.message)
+      setWarning(err.message);
+    }
+  }
+
+  return (
+    <>
+      <Card bg='light'>
+        <Card.Body>
+          <h2>Login</h2>Enter your login information below:
+        </Card.Body>
+      </Card>
+      <br />
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>User:</Form.Label>
+          <Form.Control
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            type='text'
+            id='userName'
+            name='userName'
+          />
+        </Form.Group>
+        <br />
+        <Form.Group>
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type='password'
+            id='password'
+            name='password'
+          />
+        </Form.Group>
+        <br />
+        <Button variant='primary' className='pull-right' type='submit'>
+          Login
+        </Button>
+        <br />
+        <br />
+        {warning && <Alert variant='danger'>{warning}</Alert>}
+      </Form>
+    </>
+  );
+}
