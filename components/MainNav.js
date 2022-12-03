caa04bdc415afb74c761c5a8844f3012907da140
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-12-02 22:29:40 -0500

    feature: nav bar component updated to accomodate the site's new account registration and login
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- components/MainNav.js
+++ components/MainNav.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import { useAtom } from 'jotai';
 import { searchHistoryAtom } from '../globals/store';
 
+import { readToken, removeToken } from '../lib/authenticate';
 import { addToHistory } from '../lib/userData';
 
 import Link from 'next/link';
@@ -14,6 +15,8 @@ export default function MainNav(props) {
   const [isExpanded, setIsExpanded] = useState(false);
   const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
 
+  const token = readToken();
+
   const submitSearchField = async (e) => {
     e.preventDefault(); // prevent the browser from automatically submitting the form
     setIsExpanded(false);
@@ -22,6 +25,12 @@ export default function MainNav(props) {
 
     setSearchHistory(await addToHistory(queryString));
     router.push(`/artwork?${queryString}`);
+  };
+
+  const logout = () => {
+    setIsExpanded(false);
+    removeToken();
+    router.push(`/login`);
   };
 
   const toggleNavbar = () => {
@@ -47,48 +56,86 @@ export default function MainNav(props) {
                   Home
                 </Nav.Link>
               </Link>
-              <Link href='/search' passHref legacyBehavior>
-                <Nav.Link
-                  active={router.pathname === '/search'}
-                  onClick={(e) => {
-                    setIsExpanded(false);
-                  }}
-                >
-                  Advanced Search
-                </Nav.Link>
-              </Link>
+              {token && (
+                <Link href='/search' passHref legacyBehavior>
+                  <Nav.Link
+                    active={router.pathname === '/search'}
+                    onClick={(e) => {
+                      setIsExpanded(false);
+                    }}
+                  >
+                    Advanced Search
+                  </Nav.Link>
+                </Link>
+              )}
             </Nav>
-            &nbsp;
-            <Form
-              className='d-flex'
-              onChange={(e) => setSearchField(e.target.value)}
-              onSubmit={(e) => submitSearchField(e)}
-            >
-              <Form.Control
-                className='me-2'
-                type='search'
-                placeholder='Search'
-                aria-label='Search field'
-                defaultValue={searchField}
-              />
-              <Button variant='outline-info' type='submit'>
-                Search
-              </Button>
-            </Form>
-            &nbsp;
+            {!token && (
+              <Nav>
+                <Link href='/register' passHref legacyBehavior>
+                  <Nav.Link
+                    active={router.pathname === '/register'}
+                    onClick={(e) => {
+                      setIsExpanded(false);
+                    }}
+                  >
+                    Register
+                  </Nav.Link>
+                </Link>
+                <Link href='/login' passHref legacyBehavior>
+                  <Nav.Link
+                    active={router.pathname === '/login'}
+                    onClick={(e) => {
+                      setIsExpanded(false);
+                    }}
+                  >
+                    Login
+                  </Nav.Link>
+                </Link>
+              </Nav>
+            )}
+            {token && (
+              <>
+                &nbsp;
+                <Form
+                  className='d-flex'
+                  onChange={(e) => setSearchField(e.target.value)}
+                  onSubmit={(e) => submitSearchField(e)}
+                >
+                  <Form.Control
+                    className='me-2'
+                    type='search'
+                    placeholder='Search'
+                    aria-label='Search field'
+                    defaultValue={searchField}
+                  />
+                  <Button variant='outline-info' type='submit'>
+                    Search
+                  </Button>
+                </Form>
+                &nbsp;
+              </>
+            )}
             <Nav>
-              <NavDropdown title='User Name' id='basic-nav-dropdown'>
-                <Link href='/favorites' passHref legacyBehavior>
-                  <NavDropdown.Item active={router.pathname === '/favorites'} onClick={toggleNavbar}>
-                    Favorites
-                  </NavDropdown.Item>
-                </Link>
-                <Link href='/history' passHref legacyBehavior>
-                  <NavDropdown.Item active={router.pathname === '/history'} onClick={toggleNavbar}>
-                    History
-                  </NavDropdown.Item>
-                </Link>
-              </NavDropdown>
+              {token && (
+                <NavDropdown title={token.userName} id='basic-nav-dropdown'>
+                  <Link href='/favorites' passHref legacyBehavior>
+                    <NavDropdown.Item active={router.pathname === '/favorites'} onClick={toggleNavbar}>
+                      Favorites
+                    </NavDropdown.Item>
+                  </Link>
+                  <Link href='/history' passHref legacyBehavior>
+                    <NavDropdown.Item active={router.pathname === '/history'} onClick={toggleNavbar}>
+                      History
+                    </NavDropdown.Item>
+                  </Link>
+                  <NavDropdown.Divider />
+                  <Link href='/history' passHref legacyBehavior>
+                    <NavDropdown.Item active={router.pathname === '/history'} onClick={logout}>
+                      Logout
+                    </NavDropdown.Item>
+                  </Link>
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
